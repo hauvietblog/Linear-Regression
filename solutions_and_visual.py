c7b345287533cb8fd46c8b5de5cb541a612cdb7c
HẬU <72483300+hauvi9x@users.noreply.github.com>
2023-02-03 22:56:21 +0700

    Update solutions_and_visual.py

--- solutions_and_visual.py
+++ solutions_and_visual.py
@@ -1,12 +1,16 @@
 from Visualizing_Population_VIETNAM import X,y
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import linear_model
 one = np.ones([X.shape[0],1])
 Xbar = np.concatenate((one,X), axis = 1)
 A = np.dot(Xbar.T,Xbar)
 b = np.dot(Xbar.T, y)
 w = np.dot(np.linalg.pinv(A),b)
 print('w = ',w)
+regr = linear_model.LinearRegression(fit_intercept=False)
+regr.fit(Xbar,y)
+print('solution by scikit-learn :',regr.coef_)
 w_0 = w[0][0]
 w_1 = w[1][0]
 x0 = np.linspace(1950, 2020)
